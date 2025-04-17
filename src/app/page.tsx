67ac1a19a76c5d9e8f0129e895db28217b950ffc
captainTigerYT <raunakmanna43@gmail.com>
2025-04-17 12:32:30 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import CompanyCards from "@/components/CompanyCards";
+import React from "react";
+
+const page = () => {
+  return <div>
+    <CompanyCards />
+  </div>;
+};
+
+export default page;
