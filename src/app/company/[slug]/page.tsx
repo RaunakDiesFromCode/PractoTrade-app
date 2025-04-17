e57107a4bf8390df516f161e5aeb3613f197a8bf
captainTigerYT <raunakmanna43@gmail.com>
2025-04-18 01:48:54 +0530

    ESlint rule fix

--- src/app/company/[slug]/page.tsx
+++ src/app/company/[slug]/page.tsx
@@ -1,8 +1,11 @@
-// app/[slug]/page.tsx
-
 import StockPageClient from "@/components/StockPageClient";
 
+type PageProps = {
+  params: {
+    slug: string;
+  };
+};
 
-export default function Page({ params }: { params: { slug: string } }) {
+export default function Page({ params }: PageProps) {
   return <StockPageClient slug={params.slug} />;
 }
