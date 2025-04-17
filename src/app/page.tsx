d59a00248f4984cb36b3eefa75e6250484979e3f
captainTigerYT <raunakmanna43@gmail.com>
2025-04-17 21:12:03 +0530

    updated website adress structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
-import Homepage from "@/components/Homepage";
-import React from "react";
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import React from 'react'
 
 const page = () => {
-  return <div>
-    <Homepage />
-  </div>;
-};
+  return (
+    <div className='w-screen h-[84vh] px-20 pt-5'>landing page
+      <Button asChild variant="outline">
+        <Link href="/home">
+          <span>Home</span>
+        </Link>
+      </Button>
+    </div>
+  )
+}
 
-export default page;
+export default page
