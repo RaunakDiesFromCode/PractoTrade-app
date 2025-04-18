43ebc853f433d077e706b5c2bc33efc4f200d94c
Swarnabha19 <swarnabhasarker@gmail.com>
2025-04-18 20:43:55 +0530

    minor changes in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,12 +11,8 @@ import { Input } from "./ui/input";
 const Navbar = () => {
   const { theme, setTheme } = useTheme();
   return (
-<<<<<<< HEAD
     // <div className="w-full flex justify-between items-center bg-background/60 backdrop-blur-md shadow-md px-30  border border-white/10 h-fit">
-    <div className="w-full flex justify-between items-center bg-black/70 backdrop-blur-lg  px-30 py-0.5  border border-white/15 h-[70px]">
-=======
-    <div className="w-full flex justify-between items-center bg-background/60 backdrop-blur-md shadow-md px-30  border border-white/10 h-[70px] ">
->>>>>>> 08809e2757cbcc1101473c24f0836075278d3b1b
+    <div className="w-full flex justify-between items-center bg-background/50 backdrop-blur-lg  px-30 py-0.5  border border-white/15 h-[70px]">
       {/* Left Section: Logo, Home, News */}
       <div className="flex items-center gap-5">
         <Image
